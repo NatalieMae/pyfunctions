ecf0fdc4f692c895ccbf87d43c4b697688b4f07e
Natalie <nataliemae263@gmail.com>
2022-06-27 19:15:01 -0500

    3 down more to go

---
+++ functionsP4.py
@@ -0,0 +1,16 @@
+numbers = [4, 45, 88, -5]
+
+max_number = max(numbers)
+print(max_number)
+
+def mult_list(x):
+    return x*3;
+num1=5
+num2=10
+print("The product is: ", mult_list(num1 * num2) )
+
+def rev_string(x):
+    return x[::-1]
+my_text = rev_string("How many times can you write HannaH backwards? ")
+print(my_text)
+
